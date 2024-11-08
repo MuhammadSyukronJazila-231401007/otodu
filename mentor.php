f4af730fb687eb882e6af949809fd743ecf604ea
anuGrahbodi <sibaranianugrah@gmail.com>
2024-11-08 23:38:03 +0700

    Menambah Layout PopUp Awal pada saat membeli jasa mentor

--- mentor.php
+++ mentor.php
@@ -95,6 +95,339 @@ $koin = ambilData("SELECT koin FROM users WHERE id = $id");
           height: 350px;
           width: 350px; 
       }
+      .modal-backdrop {
+            background-color: #1F2844;
+            opacity: 100%;
+            /* awalnya 90% */
+        }
+
+        .modal {
+            background-color: #1F2844;
+            opacity: 100%;
+            /* awalnya 90% */
+        }
+
+        .modal-content {
+            background-color: #1F2844;
+            opacity: 100%;
+            /* awalnya 90% */
+        }
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: "Rethink Sans";
+        }
+
+        body {
+            /* min-height: 100vh; */
+            background: white;
+            color: white;
+            background-size: cover;
+            background-position: center;
+        }
+
+        .top-element {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            z-index: 1000;
+            /* Supaya elemen ini berada di atas elemen lainnya */
+            background-color: white;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        .side-bar {
+            background: #375679;
+            backdrop-filter: blur(15px);
+            width: 250px;
+            height: 100vh;
+            position: fixed;
+            top: 0;
+            left: -250px;
+            overflow-y: auto;
+            z-index: 2000;
+            /* Menambahkan overflow-y untuk scroll */
+            transition: 0.6s ease;
+            transition-property: left;
+        }
+
+        /* Menampilkan scrollbar pada .side-bar */
+        .side-bar::-webkit-scrollbar {
+            width: 8px;
+            /* Lebar scrollbar */
+        }
+
+        .side-bar::-webkit-scrollbar-thumb {
+            background-color: #6A6A6A;
+            /* Warna thumb scrollbar */
+            border-radius: 4px;
+        }
+
+        .side-bar::-webkit-scrollbar-track {
+            background: #434041;
+            /* Warna track scrollbar */
+        }
+
+        .side-bar.active {
+            left: 0;
+        }
+
+        .side-bar .menu {
+            width: 100%;
+            margin-top: 30px;
+        }
+
+        .side-bar .menu .item {
+            position: relative;
+            cursor: pointer;
+        }
+
+        .side-bar .menu .item a {
+            color: #fff;
+            font-size: 0.9rem;
+            text-decoration: none;
+            display: flex;
+            align-items: center;
+            padding: 2vh 1.5vw;
+            border-radius: 10px;
+        }
+
+        .sub-btn {
+            font-weight: 600;
+            margin: 0 0.9vw;
+        }
+
+        .side-bar .menu .item a:hover {
+            background: #455E95;
+            transition: 0.3s ease;
+        }
+
+        .side-bar .menu .item i {
+            margin-right: 15px;
+        }
+
+        .side-bar .menu .item a .dropdown {
+            position: absolute;
+            right: 0;
+            transition: 0.3s ease;
+            padding-right: 0.8vw;
+        }
+
+        .side-bar .menu .item .sub-menu {
+            background: #3E5A86;
+            display: none;
+            margin: 0 0.9vw;
+            border-radius: 10px;
+        }
+
+        .side-bar .menu .item .sub-menu a {
+            padding-left: 2.5vw;
+        }
+
+        .side-bar .menu .item .sub-btn.active {
+            background-color: #4D62A5;
+            color: #fff;
+        }
+
+        .side-bar .menu .item .sub-menu .sub-item.active {
+            background-color: #6B7FA7;
+            color: #fff;
+        }
+
+        .rotate {
+            transform: rotate(-180deg);
+        }
+
+        .menu-btn {
+            color: rgb(255, 255, 255);
+            font-size: 1.5rem;
+            margin: 1.2px;
+            margin-left: 3vw;
+            margin-right: 3vw;
+            cursor: pointer;
+        }
+
+        .main {
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 50px;
+        }
+
+        header {
+            background: #5E88B6;
+        }
+
+        .close-btn {
+            position: absolute;
+            color: #fff;
+            font-size: 1.1rem;
+            right: 0px;
+            margin: 15px;
+            cursor: pointer;
+        }
+
+        #judul-materi {
+            padding-top: 2vw;
+            padding-left: 1vw;
+        }
+
+        #nama-materi {
+            padding-top: 0.5vw;
+            padding-left: 1vw;
+            padding-bottom: 1vw;
+            font-weight: 700;
+            font-size: 1em;
+        }
+
+        .menu p {
+            margin-left: 1vw;
+            margin-top: 1vw;
+            margin-bottom: 0.5vw;
+        }
+
+        /* Navbar */
+        .navbar {
+            background-color: white;
+            padding-inline-start: 1.5vw;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            padding: 1.5vw;
+            align-items: center;
+        }
+
+        .nav-menu {
+            margin: 0;
+            padding: 0.8vw 2vw;
+            position: relative;
+            align-self: center;
+            font-size: 1.2vw;
+            cursor: pointer;
+            font-family: 'Rethink Sans';
+            text-decoration: none;
+            color: #4D62A5;
+        }
+
+        .nav-menu:not(:last-child)::after {
+            content: '';
+            position: absolute;
+            right: 0;
+            top: 0;
+            width: 1px;
+            height: 100%;
+            background-color: #4D62A5;
+            transform: translateX(50%);
+            /* Memindahkan garis ke tengah-tengah jarak antara elemen a */
+        }
+
+        .nav-menu:hover {
+            background-color: #4D62A5;
+            color: white;
+            font-weight: 450;
+        }
+
+        #nav-main {
+            background-color: #4D62A5;
+            color: white;
+        }
+
+        .logo img {
+            width: 10vw;
+            margin-right: 1vw;
+        }
+
+        .container {
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+        }
+
+        /* End of Navbar */
+
+        /* .text {
+            padding: 1.2vw; 
+            background-color: #375679;
+        } */
+
+        .head {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100vw;
+            z-index: 1000;
+        }
+
+        table td {
+            border: 1px;
+        }
+
+        /* css konten halaman (kode anugrah) */
+        .box {
+            height: 4.5vw;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 1.4vw;
+        }
+
+        .boxleft {
+            color: white;
+            display: flex;
+            font-size: 1.4vw;
+            flex-direction: column;
+        }
+
+        .full-row {
+            background-color: #375679;
+            padding: 0;
+            /* Remove horizontal padding */
+        }
+
+        .box-4 {
+            padding: 3vw;
+            display: flex;
+            flex-direction: column;
+            gap: 1vw;
+            /* Jarak antar elemen */
+        }
+
+        .box-4 .inner-box {
+            background-color: #C2C5CD;
+            color: #F6F7FA;
+            padding: 0.8vw;
+            font-family: 'Nunito Sans', sans-serif;
+            margin-bottom: 0;
+            /* Hilangkan margin bawah */
+        }
+
+        .box-5 {
+            background-color: #d36bff;
+        }
+
+        .inner-box {
+            border-radius: 1vw;
+        }
+
+        .modal-backdrop {
+            background-color: #1F2844;
+            opacity: 100%;
+            /* awalnya 90% */
+        }
+
+        .modal {
+            background-color: #1F2844;
+            opacity: 100%;
+            /* awalnya 90% */
+        }
+
+        .modal-content {
+            background-color: #1F2844;
+            opacity: 100%;
+            /* awalnya 90% */
+        }
 
   </style>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -342,11 +675,121 @@ $koin = ambilData("SELECT koin FROM users WHERE id = $id");
             </tr>
             <tr>
               <td colspan="3" style="text-align: end; padding: 0 0 4px 0; margin-top: 0;">
-                <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#pesan1"
-                  style="color: white; font-size: 12px; margin-right: 1vw;">
-                  Pesan Mentor
+                <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                  style="color: white; font-size: 12px; margin-right: 1vw; ">
+                  Pesan Jasa
                 </button>
 
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered modal-xl">
+                      <div class="modal-content">
+                          <div style="text-align: right; margin-right: 0.8vw;">
+                              <button type="button" data-bs-dismiss="modal" aria-label="Close"
+                                  style="margin-top: 0.7vw; margin-right: 0.7vw; background-color: white; border-radius: 5vw; font-size: 15px; padding: 0px 0.2vw"> &nbsp;X&nbsp;</button>
+                          </div>
+                          <div class="modal-body">
+                              <div style="display: flex; background-color: #375679; border-radius: 0.5vw; margin-bottom: 2vw;">
+                                  <div style="margin-top: 0.8vw; color: #F6F7FA; border-radius: 4vw; font-size: 1.5vw; padding-left: 0.25vw; padding-right: 0.25vw; margin: 1.2vw 1vw 1.2vw 2.5vw;">
+                                    Pesan jasa mentor OTODU
+                                  </div>
+                              </div>
+          
+                              <div style="display: flex; justify-content: space-around">
+                                  <div style="width: 40vw; height: 30vw; background-color: white; border-radius: 0.5vw;">
+                                      <center>
+                                        <div style="display: flex; align-items: center;">
+                                          <img src="image/user.png" alt="User Image" style="width: 50px; margin-right: 15px;">
+                                          
+                                          <div style="display: flex; flex-direction: column; border-bottom: 1px solid black;">
+                                            <p>Mentor Skibdi L., S.Komedi</p>
+                                            
+                                            <div style="display: flex; align-items: center;">
+                                              <img src="image/pinhitam.png" alt="Location Pin" style="width: 20px; margin-right: 5px;">
+                                              <div>690 m</div>
+                                            </div>
+                                            
+                                          </div>                                          
+                                        </div>
+                                        <div style="border-bottom: 1px solid #3A425A; color: #3A425A; padding-bottom: 4px; display: flex; align-items: center;">
+                                          <font>Mengajar</font>
+                                      </div>
+                                  
+                                      <div style="display: flex; gap: 10px; align-items: center; margin-top: 1vw;">
+                                          <span style="padding: 10px 20px; background-color: #FF6F00; color: white; border-radius: 5px; font-size: 16px;">Matematika</span>
+                                          <span style="padding: 10px 20px; background-color: #388E3C; color: white; border-radius: 5px; font-size: 16px;">Dasar Pemrograman</span>
+                                          <span style="padding: 10px 20px; background-color: #1976D2; color: white; border-radius: 5px; font-size: 16px;">UTBK</span>
+                                      </div>
+
+                                    <div style="border-bottom: 1px solid #3A425A; color: #3A425A; padding-bottom: 4px; display: flex; align-items: center;">
+                                        <font>Ketersediaan</font>
+                                    </div>
+                                
+                                    <div style="display: flex; flex-direction: column; gap: 10px; margin-top: 1vw;">
+                                      <span style="padding: 5px 10px; background-color: #FF6F00; color: white; border-radius: 5px; font-size: 16px;">Luring @ 13:00 - 16:00, 16:30 - 19:00</span>
+                                      <span style="padding: 5px 10px; background-color: #388E3C; color: white; border-radius: 5px; font-size: 16px;">Daring @ 13:00 - 16:00, 16:30 - 19:00, 19:30 - 22:30</span>
+                                  </div>                                  
+                                    
+                                  <div style="border-bottom: 1px solid #3A425A; color: #3A425A; padding-bottom: 4px; display: flex; align-items: center;">
+                                    <font>Riwayat studi</font>
+                                </div>
+                                
+                                <div style="display: flex; flex-direction: column; gap: 10px; margin-top: 10px; align-items: flex-start;">
+                                    <p style="font-size: 16px; color: #3A425A; margin: 0;">Sarjana Ilmu Komedi - Universitas Kampus Institut</p>
+                                    <p style="font-size: 16px; color: #3A425A; margin: 0;">Magister Sastra Mesin - Institut Teknologi Dimensi</p>
+                                </div>                                
+
+                                      </center>
+                                  </div>
+                                  <div style="width: 40vw; height: 30vw; background-color: white; border-radius: 0.5vw;">
+                                      <div style="margin-top: 1vw; text-align: center;">
+
+                                        <div style="color: #3A425A; padding-bottom: 4px; display: flex; align-items: center;">
+                                          <font>Materi yang ingin dipelajari</font>
+                                      </div>
+                                      
+                                      <div style="margin-top: 10px; display: flex; flex-direction: column; align-items: flex-start;">
+                                          <select style="padding: 10px 20px; font-size: 16px; color: #3A425A; border: 1px solid #3A425A; border-radius: 5px; width: 100%;">
+                                              <option value="matematika" style="font-weight: bold;">Matematika</option>
+                                          </select>
+                                      </div>
+
+                                      <div style="margin-top: 1vw; text-align: center;">
+
+                                        <div style="color: #3A425A; padding-bottom: 4px; display: flex; align-items: center;">
+                                          <font>Topik</font>
+                                      </div>
+                                      
+                                      <div style="margin-top: 10px; display: flex; flex-direction: column; align-items: flex-start;">
+                                          <select style="padding: 10px 20px; font-size: 16px; color: #3A425A; border: 1px solid #3A425A; border-radius: 5px; width: 100%;">
+                                              <option value="Fungsi" style="font-weight: bold;">Matematika</option>
+                                          </select>
+                                      </div>
+
+                                      <div style="margin-top: 1vw; text-align: center;">
+
+                                        <div style="color: #3A425A; padding-bottom: 4px; display: flex; align-items: center;">
+                                          <font>Fungsi</font>
+                                      </div>
+                                      
+                                      <div style="margin-top: 10px; display: flex; flex-direction: column; align-items: flex-start;">
+                                          <select style="padding: 10px 20px; font-size: 16px; color: #3A425A; border: 1px solid #3A425A; border-radius: 5px; width: 100%;">
+                                              <option value="matematika" style="font-weight: bold;">Daring</option>
+                                          </select>
+                                      </div>
+                                      
+                                      <button style="margin-top: 20px; padding: 10px 0; background-color: #FF6F00; color: white; border: none; border-radius: 5px; font-size: 16px; width: 100%; text-align: center;">
+                                        Pesan
+                                    </button>
+                                      
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+          
+                      </div>
+                  </div>
+              </div>
+                
                 <div class="modal fade" id="pesan1" data-bs-backdrop="static" data-bs-keyboard="false"
                   tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                   <div class="modal-dialog">
